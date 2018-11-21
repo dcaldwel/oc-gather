424b2d917a7a2c7395d7b2fd208189dfeca47962
dcaldwel <dcaldwel@redhat.com>
2018-11-21 11:20:46 -0500

    version output

--- oc-gather.sh
+++ oc-gather.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-echo 'Starting collection...'
+# Get output file string and create file. Exit if already exists.
+
+getopts f: OPT
+
+case $OPT in 
+  f) 
+    if test -e $OPTARG; then
+      echo $OPTARG 'already exists. Will not overwrite.'
+      exit 1
+    else
+      echo 'Creating' $OPTARG
+      touch $OPTARG
+    fi
+  ;;
+
+  ?)
+    printf '\nUsage: oc-gather.sh -f <output filename>\n\n'
+    exit 1
+  ;;
+esac
+
+
+#
+# Gather stuff
+#
+
+printf "\nGathering...\n\n"
+echo 'oc-gather started:' >> $OPTARG; date >> $OPTARG
+printf "\n\n--=oc commands=--\n\n" >> $OPTARG
+
+oc version | tee -a $OPTARG
+
