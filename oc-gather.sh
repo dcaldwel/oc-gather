2521062cad4c938d74af6839958772275b92f4f7
dcaldwel <dcaldwel@redhat.com>
2018-12-04 04:51:00 -0500

    Added some comments

--- oc-gather.sh
+++ oc-gather.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-##############
-# Gather info#
-##############
+##############################################################################
+# oc-gather.sh                                                               #
+# Utility to collect log and oc command output for troubleshooting OpenShift #
+# https://github.com/dcaldwel/oc-gather                                      #
+##############################################################################
 
-# Add or remove a test function here to enable or disable the test.
+# Add or remove a test function to or from FLAGS here to enable or disable the test.
 # Test functions are defined in './oc-gather-lib.sh.'
+# New functions should be prefixed with 'gather_' to distinguish then from shell commands and binaries.
 FLAGS="gather_boilerplate gather_misc gather_version gather_nodes gather_etcd gather_storage gather_network gather_deployment_errors gather_events gather_docker"
 
 # Load test functions library
